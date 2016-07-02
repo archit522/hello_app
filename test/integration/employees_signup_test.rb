3f4aca39582b1c3be9f7ec21d844fc8042717e52
Archit Aggarwal <archit522@gmail.com>
2016-07-02 16:58:15 +0530

    Fininsh user signup

---
+++ test/integration/employees_signup_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class EmployeesSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'Employee.count' do
+      post employees_path, employee: { name:  "",email: "employee@invalid", password: "foo", password_confirmation: "bar" }
+    end
+    assert_template 'employees/new'
+  end
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'Employee.count', 1 do
+      post_via_redirect employees_path, employee: { name:  "Example Employee",
+                                            email: "employee@example.com", password: "foobar", password_confirmation: "foobar" }
+    end
+    assert_template 'employees/show'
+  end
+end
